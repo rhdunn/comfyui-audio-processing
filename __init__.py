c08b2217957f544a3f3552a14db1e0299eae7942
Reece H. Dunn <msclrhd@gmail.com>
2024-07-26 19:01:22 +0100

    Load Audio From Path

--- __init__.py
+++ __init__.py
@@ -1,7 +1,11 @@
 # Copyright (C) 2024 Reece H. Dunn. SPDX-License-Identifier: GPL-3
 
+from .io_nodes import LoadAudio
+
 NODE_CLASS_MAPPINGS = {
+    "ComfyAudio.LoadAudio": LoadAudio,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
+    "ComfyAudio.LoadAudio": "Load Audio From Path",
 }
---
+++ io_nodes.py
@@ -0,0 +1,35 @@
+# Copyright (C) 2024 Reece H. Dunn. SPDX-License-Identifier: GPL-3
+
+import torchaudio
+import os
+
+
+class LoadAudio:
+    SUPPORTED_FORMATS = ('.wav', '.mp3', '.ogg', '.flac', '.aiff', '.aif')
+
+    @classmethod
+    def INPUT_TYPES(s):
+        return {"required": {"audio": ("STRING",{"multiline":False})}}
+
+    CATEGORY = "AudioProcessing"
+
+    RETURN_TYPES = ("AUDIO", )
+    FUNCTION = "load"
+
+    def load(self, audio):
+        waveform, sample_rate = torchaudio.load(audio)
+        audio = {"waveform": waveform.unsqueeze(0), "sample_rate": sample_rate}
+        return (audio, )
+
+    @classmethod
+    def IS_CHANGED(s, audio):
+        m = hashlib.sha256()
+        with open(audio, 'rb') as f:
+            m.update(f.read())
+        return m.digest().hex()
+
+    @classmethod
+    def VALIDATE_INPUTS(s, audio):
+        if not os.path.exists(audio) or not audio.endswith(LoadAudio.SUPPORTED_FORMATS):
+            return "Invalid audio file: {}".format(audio)
+        return True
