4c97b68ed8b9210cc9eb91a999e1e6dfc0c7789c
Reece H. Dunn <msclrhd@gmail.com>
2024-07-26 22:35:56 +0100

    Power Spectrogram

--- __init__.py
+++ __init__.py
@@ -2,16 +2,18 @@
 
 from .io_nodes import LoadAudio
 from .plot_nodes import PlotWaveform
-from .spect_nodes import Spectrogram
+from .spect_nodes import (Spectrogram, PowerSpectrogram)
 
 NODE_CLASS_MAPPINGS = {
     "ComfyAudio.LoadAudio": LoadAudio,
     "ComfyAudio.PlotWaveform": PlotWaveform,
+    "ComfyAudio.PowerSpectrogram": PowerSpectrogram,
     "ComfyAudio.Spectrogram": Spectrogram,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
     "ComfyAudio.LoadAudio": "Load Audio From Path",
     "ComfyAudio.PlotWaveform": "Plot Waveform",
+    "ComfyAudio.PowerSpectrogram": "Power Spectrogram",
     "ComfyAudio.Spectrogram": "Spectrogram",
 }
